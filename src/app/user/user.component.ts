5fc088644562c5acba7cb37d857fc3715cfa37b3
sylwia <sylwiavv@gmail.com>
2024-05-24 11:18:59 +0200

    Add dynamic user data

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {DUMMY_USERS} from "../data/DUMMY_USERS"
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+console.log(randomIndex, "randomIndex");
 
 @Component({
   selector: 'app-user',
@@ -8,5 +12,5 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-
+  selectedUser = DUMMY_USERS[randomIndex]
 }
