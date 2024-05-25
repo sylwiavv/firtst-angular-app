fe03ad7f0d7ce28028877118e4dfd289a98eb2fe
sylwia <sylwiavv@gmail.com>
2024-05-25 12:39:34 +0200

    Add tasks component, and show user's name and avatar

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,8 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, Input, computed, signal, input, Output, output, EventEmitter} from '@angular/core';
 import {DUMMY_USERS} from "../data/DUMMY_USERS"
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
-console.log(randomIndex, "randomIndex");
 
 @Component({
   selector: 'app-user',
@@ -12,5 +11,38 @@ console.log(randomIndex, "randomIndex");
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+  // selectedUser = signal(DUMMY_USERS[randomIndex])
   selectedUser = DUMMY_USERS[randomIndex]
+
+  // avatarPath = computed(() =>'assets/users/' + this.selectedUser().avatar )
+  @Input({required: true}) id!: string
+  @Input({required: true}) avatar!: string;
+  @Input({required: true}) name!: string;
+
+  @Output() select = new EventEmitter()
+
+  // select = output<string>()
+
+  get avatarPath() {
+    return 'assets/users/' + this.avatar
+  }
+
+
+
+  onSelectUser() {
+    this.select.emit(this.id)
+    // const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+    // this.selectedUser = set(DUMMY_USERS[randomIndex])
+  }
+
+  // SIGNALS
+  // avatar = input.required<string>()
+  // name = input.required<string>()
+
+
+
+  // avatarPath = computed(() => {
+  //   return 'assets/users/' + this.avatar()
+  // })
+
 }
